8889b9c80dece4ce2012ee7adb92ee23fe140a7e
urmommine <aljaprant12@gmail.com>
2025-05-05 18:01:45 +0700

    add migration dan models

---
+++ app/Models/Treatment.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Treatment extends Model
+{
+    public function patient(): BelongsTo
+    {
+        return $this->belongsTo(Patient::class);
+    }
+}
